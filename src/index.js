ec8c2ebb3100e7320cfc472c4439021b44f3e3a9
Prakhar Goel <itsprakharg07@gmail.com>
2020-12-08 19:53:00 +0530

    new change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from "./App"
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import {Route,Switch} from 'react-router-dom'
 import Mobile from './Mobile'
 import Electricity from './Electricity'
 import Recharge from './Recharge'
 
-ReactDOM.render(<BrowserRouter>
+ReactDOM.render(<HashRouter>
 <Switch>
     <Route exact path='/' component={App} />
     <Route exact path='/PaytmRedux' component={App} />
@@ -15,4 +15,4 @@ ReactDOM.render(<BrowserRouter>
     <Route exact path='/electricity' component={Electricity} />
     <Route exact path='/recharge' component={Recharge} />
 </Switch>
-</BrowserRouter>,document.getElementById('root'));
+</HashRouter>,document.getElementById('root'));
